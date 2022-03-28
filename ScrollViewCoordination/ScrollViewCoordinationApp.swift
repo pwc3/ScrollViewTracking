751d34e7bcddc565327fdefbedd4fdb96c257e50
Paul Calnan <pwc3@users.noreply.github.com>
2022-03-28 16:12:50 -0400

    Initial Commit

---
+++ ScrollViewCoordination/ScrollViewCoordinationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewCoordinationApp.swift
+//  ScrollViewCoordination
+//
+//  Created by Paul Calnan on 3/28/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewCoordinationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
